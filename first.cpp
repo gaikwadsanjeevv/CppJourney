bc60aecd1b1aa7f9797055592a9af0fbe5781954
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-11-25 23:22:20 -0800

    Update first.cpp
    explanation of std :: etc

--- first.cpp
+++ first.cpp
@@ -5,7 +5,9 @@
 int main()
 {
 
+std::cout << "Hello World";
 
+  //std -> namespace, :: -> Scope resolution, cout -> Object,  << -> stream insertion operator, or write using namespace std; -> to avoid writing std
 
 
 
